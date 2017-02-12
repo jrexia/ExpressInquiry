60d610b1262a3a58357048559f81dd42866fc586
jrexia <jrexia@163.com>
2017-02-12 15:10:41 +0800

    快递单号查询（版本2）

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 window.onload = function() {
     // alert($('#postid').offset().left)
     // $(".queryContext").css("left", $('#postid').offset().left - $('.queryContext').offset().left + "px");
-    $('#postid').focus(function() {
+    var selectid = document.querySelector("#selectid");
+    var selectbtn = document.querySelector("#select-btn");
+    var data = [];
+    $('.ser-txt').focus(function() {
         var email_txt = $(this).val();
         if (email_txt == this.defaultValue) {
             $(this).val("");
         }
 
     });
-    $('#postid').blur(function() {
+    $('.ser-txt').blur(function() {
         var email_txt = $(this).val();
         if (email_txt == "") {
             $(this).val(this.defaultValue);
@@ -16,20 +19,69 @@ window.onload = function() {
 
     });
 
+    // $("#select-btn").click(function() {
+    //     $("#company").css("display", "block");
+    // });
+    selectbtn.onclick = function(e) {
+        $("#company").toggle();
+        e.stopPropagation();
+    };
+    document.onclick = function(e) {
+        $("#company").hide();
+        // e.stopPropagation();
+    };
+    // 获取快递公司部分开始
+    $.ajax({
+        type: "get",
+        url: "https://route.showapi.com/64-20?maxSize=100000&showapi_appid=31610&showapi_sign=794da37ef6d548bdb3faf07de393bc6d",
+
+        dataType: "json",
+        success: function(json) {
+            for (var i = 0; i < json.showapi_res_body.expressList.length; i++) {
+                company.loadMore(json.showapi_res_body.expressList[i]);
+            }
+
+            console.log(json.showapi_res_body.expressList);
+        },
+        error: function(e) {
+            console.log(e);
+        }
+    });
+
+    var company = new Vue({
+        el: '#company',
+        data: {
+            items: [],
+        },
+        methods: {
+            loadMore: function(moreData) {
+
+                this.items.push(moreData);
+            },
+            toggle: function(index) {
+                // console.info(index);
+                selectid.value = this.items[index].expName;
+                data[0] = this.items[index].simpleName;
+                // data[3] = this.items[index].expName;
+            }
+        }
+    });
+
+    // 获取快递公司部分结束
+    // 获取查询结果部分开始
     var app;
     $(".search .btn-search").click(function() {
         // alert($("#postid").val())
         $.ajax({
             type: "get",
-            url: "https://route.showapi.com/880-1?nu=" + $("#postid").val() + " &showapi_appid=31976&showapi_sign=07286aedccaf4f5d9cd3c9e92e6fe036",
-
+            url: "https://route.showapi.com/64-19?com=" + data[0] + "&nu=" + $("#postid").val() + "&showapi_appid=31610&showapi_sign=794da37ef6d548bdb3faf07de393bc6d",
             dataType: "json",
             success: function(json) {
                 for (var i = 0; i < json.showapi_res_body.data.length; i++) {
                     app.loadMore(json.showapi_res_body.data[i]);
                 }
 
-                console.log(json.showapi_res_body.data[0].time);
+                console.log(json.showapi_res_body.data);
             },
             error: function(e) {
                 console.log(e);
@@ -49,4 +101,5 @@ window.onload = function() {
             }
         }
     });
+    // 获取查询结果部分结束
 }
